4d9d793253b68940ccb0ff4bf042477256f45f85
Mateusz <mkaminski.contact@gmail.com>
2023-02-25 23:32:24 +0100

    added scrollSpy and improved bg images

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ const burgerBtn = document.querySelector(".burger-btn");
 const navItems = document.querySelector(".nav__items-mobile");
 const footerYear = document.querySelector(".footer-year");
 const desktopNavItems = document.querySelectorAll('.nav__item')
-const scrollSection = document.querySelectorAll('.scroll-section')
+const scrollSections = document.querySelectorAll('.scroll-section')
 
 footerYear.textContent = new Date().getFullYear();
 
@@ -11,7 +11,27 @@ const handleNav = () => {
 };
 
 
-const scrollSpy = (section) => {
-	
-}
+const scrollSpy = () => {
+    if (document.body.classList.contains("main")) {
+        const sections = [];
+
+        scrollSections.forEach((section) => {
+            if (
+                window.scrollY <=
+                section.offsetTop + section.offsetHeight - 56
+            ) {
+                sections.push(section);
+
+                const activeSection = document.querySelector(
+                    `[href*="${sections[0].id}"]`
+                );
+
+                desktopNavItems.forEach((item) => item.classList.remove("nav__item--active"));
+
+                activeSection.classList.add("nav__item--active");
+            }
+        });
+    }
+};
 burgerBtn.addEventListener("click", handleNav);
+window.addEventListener('scroll', scrollSpy)
