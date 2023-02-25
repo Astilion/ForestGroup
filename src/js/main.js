7d50bbf7300ec55584a8653413d5a7d7c0ba1410
Mateusz <mkaminski.contact@gmail.com>
2023-02-25 21:51:05 +0100

    added nav__item--scroll class to make scrollspy

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 const burgerBtn = document.querySelector(".burger-btn");
 const navItems = document.querySelector(".nav__items-mobile");
 const footerYear = document.querySelector(".footer-year");
+const desktopNavItems = document.querySelectorAll('.nav__item')
+const scrollSection = document.querySelectorAll('.scroll-section')
 
 footerYear.textContent = new Date().getFullYear();
 
@@ -8,4 +10,8 @@ const handleNav = () => {
 	navItems.classList.toggle("active");
 };
 
+
+const scrollSpy = (section) => {
+	
+}
 burgerBtn.addEventListener("click", handleNav);
